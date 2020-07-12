2ef9f78aac6748c0a1636624941117dca18aae0a
junehan <junehan.dev@gmail.com>
2020-07-12 17:17:52 +0900

    ft_itox

---
+++ hexdump/includes/ft_hexdump.h
@@ -0,0 +1,1 @@
+char    *ft_itox(int key);
---
+++ hexdump/srcs/ft_itox.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+char    *ft_itox(int val)
+{
+    char    ret[3];
+    int     key;
+
+    if (val < 0 || val > 127)
+        return (NULL);
+    ret[0] = '0';
+    ret[1] = '0';
+    if (val)
+    {
+        key = val / 16;
+        ret[0] = '0' + key;
+        if (key > 9)
+            ret[0] = 'a' + (key - 10);
+        key = val % 16;
+        ret[1] = '0' + key;
+        if (key > 9)
+            ret[1] = 'a' + (key - 10);
+    }
+    ret[2] = '\0';
+    return (strdup(ret));
+}
