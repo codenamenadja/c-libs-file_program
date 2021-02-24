c39c25416c0cc18fac04b429c77c09ef17fb61ba
junehan <junehan.dev@gmail.com>
2021-02-24 17:14:14 +0900

    display_file/FIN

--- display_file/README.rst
+++ display_file/README.rst
@@ -15,14 +15,14 @@ Constraints
 
 
 Arguments
----------
+^^^^^^^^^
 
 - All files given as arguments validation test should occur.
 - Error messages have to be displayed on their reserved output followed by a new line.
 - If no argument is given, it should display File name missing.
-- If there is more than one argument, it should display Too many arguments.
+- (deprecated) If there is more than one argument, it should display Too many arguments.
 
 Error
------
+^^^^^
 
 - If the file cannot read, it should display *cannot read file.*
--- display_file/ft_display_file.c
+++ display_file/ft_display_file.c
@@ -1,45 +1,75 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 16:10:58 by junehan           #+#    #+#             */
+/*   Updated: 2021/02/24 17:10:59 by junehan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int open_handler(char *path)
-{
-    int     fd;
+#include	<unistd.h>
+#include	<sys/types.h>
+#include	<fcntl.h>
 
-    fd = open(path, O_RDONLY);
-    if(fd == -1)
-        return (-1);
-    return (fd);
-}
+#define		BUFSIZ	1024
+enum {EXIT_SUCCESS, EXIT_FAILURE};
 
+
+int	open_handler(char *path);
+ssize_t	read_handler(int fd, void *buf);
 
 int main(int argc, char *argv[])
 {
-    int i;
-    int fd;
-    ssize_t read_bytes;
-    char buf[BUFSIZ];
-    int res;
+    ssize_t rb;
+	int		fd;
+    char	buf[BUFSIZ];
+    int		reads;
 
-    i = 1;
-    res = 0;
-    if (argc == 1)
-    {
-        write(STDOUT_FILENO, "non filepath given to an argument\n", BUFSIZ);
+    reads = 1;
+	
+    if (argc == 1) {
+        write(STDOUT_FILENO, "File name missing.\n", 19);
         return (EXIT_FAILURE);
-    }
-    while(i < argc)
-    {
-        fd = open_handler(argv[i]);
-        if (fd == -1 && i++)
-            continue;
-        while((read_bytes = read(fd, buf, BUFSIZ)) > 0)
-            write(STDOUT_FILENO, buf, read_bytes);
-        res++;
-        close(fd);
-        i++;
-    }
-    return (res);
+	}
+
+    while (reads < argc) {
+		fd = open_handler(argv[reads++]);
+
+		if (fd > STDERR_FILENO) {
+			do {
+				rb = read_handler(fd, buf);
+				write(STDOUT_FILENO, buf, rb);
+			} while (rb == BUFSIZ);
+			close(fd);
+		} else {
+			write(STDOUT_FILENO, "cannot read file.\n", 18);
+		}
+
+	}
+
+	if (reads == 1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+ssize_t	read_handler(int fd, void *buf)
+{
+	return read(fd, buf, BUFSIZ);
+}
+
+int	open_handler(char *path)
+{
+    int	fd;
+
+	if (!path)
+		fd = 0;
+	else if ((fd = open(path, O_RDONLY)) <= STDERR_FILENO) {
+		close(fd);
+		fd = -1;
+	}
+
+	return (fd);
 }
